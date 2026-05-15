ab864b028a40ac18cd6d71e6ef878039e864d472
Fernando Eufrásio <fernando.epm1@gmail.com>
2026-05-14 21:03:17 -0300

    test: add new cpu helper to build the cpu in the specs

---
+++ spec/support/cpu_helper.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module CpuHelper
+  # Setup the CPU to be in a testable state.
+  def build_cpu(rom_data)
+    rom = Akane::Cartridge::Rom.new(rom_data)
+    cartridge = Akane::Cartridge.new(rom: rom)
+    wram = Akane::Gameboy::Ram.new(8192)
+    hram = Akane::Gameboy::Ram.new(127)
+    apu = Akane::Gameboy::Apu.new
+    interrupts = Akane::Gameboy::Interrupts.new
+
+    ppu = Akane::Gameboy::Ppu.new(interrupts)
+    timer = Akane::Gameboy::Timer.new(interrupts)
+    serial = Akane::Gameboy::Serial.new(interrupts)
+    joypad = Akane::Gameboy::Joypad.new(interrupts)
+
+    bus = Akane::Gameboy::Bus.new(
+      cartridge: cartridge,
+      ppu: ppu,
+      wram: wram,
+      hram: hram,
+      interrupts: interrupts,
+      apu: apu,
+      timer: timer,
+      serial: serial,
+      joypad: joypad
+    )
+
+    Akane::Gameboy::Cpu.new(bus, interrupts, -> {}, false)
+  end
+end
